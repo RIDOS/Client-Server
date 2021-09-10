4e630f30230ae26187c7ceaff15b217132d35198
RIDOS <guzairov99@bk.ru>
2021-09-10 20:51:37 +0500

    #2 Add Exit button

--- client.cpp
+++ client.cpp
@@ -8,37 +8,49 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-char message[] = "Hello there!\n"; // Сообщение пользователя.
-char buf1[sizeof(message)]; // Буфер куда попадает сообщение от пользователя.
-int fs, fr; // Переменные, в которые будут записываться результаты вызванных методов send и recv.
+char message[] = "Hello there!\n";
+char buf1[sizeof(message)];
+int fs, fr;
 
 int main() {
-	int sock; // Переменная сокета.
-	struct sockaddr_in addr; // Структура адреса клиента и сервера.
-	sock = socket(AF_INET, SOCK_STREAM, 0); // Вызываем функцию socket для получения информации о домене и типе сокета.
+	int sock;
+	struct sockaddr_in addr;
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	
-	if (sock < 0) { // Проверка на ошибки. (При 0 - ошибка)
+	if (sock < 0) {
 		perror("socket");
 		exit(1);
 	}
 	
-	addr.sin_family = AF_INET; // Записываем в структуру Internet-домен.
-	addr.sin_port = htons(3425); // Записываем в структуру порт.(Host TO Network Short)
-	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // Записываем в структуру адресс внутренней петли.
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(3425);
+	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	
-	if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) { // Пытаемся соединится с сервером. При ошибке выводим ошибку, иначе идем далее.
+	if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
 		perror("connect");
 		exit(2);
 	}
 	
-	fs = send(sock, message, sizeof(message), 0); // 
-	fr = recv(sock, buf1, sizeof(message), 0); // 
+	fs = send(sock, message, sizeof(message), 0);
+	fr = recv(sock, buf1, sizeof(message), 0);
+	printf("Server: %s", buf1);
 	
-	printf("B\n"); // Выводим переменную B и переходим на новую строку.
-	printf("\n %s\n\n\n",buf1); // ***Здесь была ошибка. [format not a string literal and no format arguments]. Исправил с помощью %s - строка символов.
-	close(sock); // Закрываем подключение.
 	
-	return 0; // Метод Main выводит 0.
+	/*---------------------- Close Server code. ----------------------*/
+	int ch;
+	char new_message[] = "stop";
+	
+	printf("\nClose Server? (1/0): ");
+	scanf("%d", &ch);
+	
+	if (ch == 1) {
+		fs = send(sock, new_message, sizeof(new_message), 0);
+		printf("\nServer has been stoped.\n");
+	}
+	
+	close(sock);
+	
+	return 0;
 }
 
 // Created by RIDOS 
--- server.cpp
+++ server.cpp
@@ -8,69 +8,63 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-char message1[] = "123456789\n"; // Сообщение сервера.
-unsigned int sz; // Переменная целово типа, которая будет принимать размер принемаемого адреса. 
+char message1[] = "123456789\n";
+unsigned int sz;
 
 int main() {
-	int sock, listener; // Переменные сокета.
-	struct sockaddr_in addr; // Структура сокета (IP, порт и внутрений адресс).
-	char buf[1024]; // Буфер обмена. В него передается 
-	int bytes_read, bytes_read1; // Первая переменная - длина буфера с записынами данными от клиента, вторая переменная - длина буфера с записынами данными от сервера.
-	listener = socket(AF_INET, SOCK_STREAM, 0); // Создаем новый сокет. В скобках аргументы 1 - для сетевого протокола IPv4, 2 - потоковый сокет, 3 - протокол.
-	if (listener < 0) { // Проверяем созданный сокет на ошибки.
+	int sock, listener;
+	struct sockaddr_in addr;
+	char buf[1024];
+	int bytes_read, bytes_read1;
+	listener = socket(AF_INET, SOCK_STREAM, 0);
+	if (listener < 0) {
 		perror("socket");
 		exit(1);
 	}
 	
-	addr.sin_family = AF_INET; // Записываем в структуру Internet-домен.
-	addr.sin_port = htons(3425); // Записываем в структуру порт.(Host TO Network Short)
-	addr.sin_addr.s_addr = htonl(INADDR_ANY); // Записываем в структуру адресс внутренней петли.
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(3425);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
-	if (bind(listener, (struct sockaddr *) &addr, sizeof(addr)) < 0) { // Присваиваем сокету имя и проверяем сам сокет на работоспособность. 
+	if (bind(listener, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
 		perror("bind");
 		exit(2);
 	}
 	
-	listen(listener, 1); // Ожидание запроса от сокета.
+	listen(listener, 1);
 	
-	while (1) { // Бесконечный цикл, для того чтобы сервер не выключался и ждал ответ от клиента.
-		sz = sizeof(addr); // Записываем размер адреса.
-		sock = accept(listener, (struct sockaddr *) &addr, &sz); // Принимаем связь с сокета.
-		write(1, message1, sock); // Записываем в сокет сообщение.
-		printf("\n"); // 
-		if (sock < 0) { // Проверяем сокет на работоспособность.
+	while (1) {
+		sz = sizeof(addr);
+		sock = accept(listener, (struct sockaddr *) &addr, &sz);
+		
+		if (sock < 0) {
 			perror("accept");
 			exit(3);
 		}
 		
-		while(1) { // Так же - бесконечный цикл.
-			bytes_read = recv(sock, buf, 1024, 0); // 
-			send(sock, buf, bytes_read, 0);
-			/*
-			 * Первый аргумент - сокет-дескриптор, в который записываются данные. 
-			 * Второй и третий аргументы - соответственно, адрес и длина буфера с записываемыми данными. 
-			 * Четвертый параметр - это комбинация битовых флагов, управляющих режимами записи.
-			 *
-			*/
+		while(1) {
+			bytes_read = recv(sock, buf, 1024, 0);
 			
-			if (bytes_read <= 0) break; // Если длина буфера равна нулю или меньше (т.е. буфер пуст), то выходим из цикла.
-			
-			sz = sizeof(addr); // Записываем новый размер адреса. 
-			sock = accept(listener, (struct sockaddr *) &addr, &sz); // Принимаем связь с сокета.
-			
-			write(1, message1, sock); // Записываем в сокет сообщение.
-			if (sock < 0) // Проверям сокет на работоспособность.
+			if (sock < 0)
 			{
 				perror("accept");
 				exit(3);
 			}
 			
-			bytes_read1 = recv(sock, message1, 1024, 0); // В переменную  bytes_read1 - записывается данные из сокета.
+			if (bytes_read <= 0) break;
+			printf("%s\n", buf);
+			send(sock, message1, bytes_read, 0);
 			
-			if (bytes_read1 <= 0) break; // Если сокет пуст, то выходим из цикла.
-			send(sock, message1, bytes_read1, 0); // Записывает данные в сокет.
+			bytes_read1 = recv(sock, buf, 1024, 0);
+			printf("%s\n", buf);
+			if (buf == "stop") {
+				printf("Server close: The server is stopped at the request of the client.");
+				close(sock);
+				exit(4);
+			}
 		}
-		close(sock); // Обрываем подключение (Закрываем сокет).
+		
+		close(sock);
 	}
 	
 	return 0;
